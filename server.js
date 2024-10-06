b6519ad093536abf9de6454cd92e5aade66a53d9
Azdan Vila Ramadhani <azdan912@gmail.com>
2024-10-06 02:33:10 +0000

    feat: render the pug template (#1)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const res = require('express/lib/response.js');
 
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index')
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
